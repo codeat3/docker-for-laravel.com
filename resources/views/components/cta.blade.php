01aedaca62fb44681823711ce3bed0f9f5d11bfc
Swapnil Sarwe <swapnilsarwe@gmail.com>
2024-09-12 00:17:08 +0530

    updated contribution link

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -2,8 +2,8 @@
     <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">You got any resources?</h2>
       <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
-        <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create PR</a>
-        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Add to the list <span aria-hidden="true">→</span></a>
+        <a href="https://github.com/codeat3/docker-for-laravel.com?tab=readme-ov-file#contribution" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Contribute</a>
+        <a href="https://github.com/codeat3/docker-for-laravel.com?tab=readme-ov-file#contribution" class="text-sm font-semibold leading-6 text-gray-900">Create a PR <span aria-hidden="true">→</span></a>
       </div>
     </div>
   </div>
